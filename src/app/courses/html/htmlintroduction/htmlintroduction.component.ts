37c494d524d9bf9cc2a52f7ef329a1ba7f5997f0
sarath-cmd <sarathredmagic@gmail.com>
2024-09-18 19:42:54 +0530

    18.9.24

--- src/app/courses/html/htmlintroduction/htmlintroduction.component.ts
+++ src/app/courses/html/htmlintroduction/htmlintroduction.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { VertnavComponent } from '../vertnav/vertnav.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { DarkthemeService } from '../../../darktheme.service';
 
 @Component({
   selector: 'app-htmlintroduction',
@@ -11,5 +12,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './htmlintroduction.component.css'
 })
 export class HtmlintroductionComponent {
-
+  isDark: boolean = false;
+  constructor(private themeService: DarkthemeService) {}
+  ngOnInit() {
+    this.themeService.isDark$.subscribe(isDark => {
+      this.isDark = isDark;
+    });
+  }
+  
 }
